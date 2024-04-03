61b579d0c618df5fa0fd676dadfbfe0da2015681
ImonHBnLT <angeloimon@outlook.com>
2024-04-03 12:33:32 -0300

    ok

--- lib/view/cad_view.dart
+++ lib/view/cad_view.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class CadView extends StatefulWidget {
   const CadView({super.key});
@@ -11,7 +12,7 @@ class CadView extends StatefulWidget {
 
 var formKey = GlobalKey<FormState>();
 var txtValor1 = TextEditingController();
-var txtValor2 = TextEditingController();
+String txtSenha="a";
 
 class _CadViewState extends State<CadView> {
   @override
@@ -45,10 +46,6 @@ class _CadViewState extends State<CadView> {
                 },
               ),
               SizedBox(height: 30),
-              TextFormField(
-
-              ),
-              SizedBox(height: 30),
               ElevatedButton(
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Colors.blue.shade100,
@@ -60,20 +57,34 @@ class _CadViewState extends State<CadView> {
                   if (formKey.currentState!.validate()) {
                     ScaffoldMessenger.of(context).showSnackBar(
                       SnackBar(
-                        content: Text('Bem vindo!'),
+                        content: Text('Senha Gerada com Sucesso!'),
                         duration: Duration(seconds: 3),
                       ),
                     );
+                    txtSenha = generateRandomString();
                   } else {
                     ScaffoldMessenger.of(context).showSnackBar(
                       SnackBar(
-                        content: Text('email ou senha inválidos.'),
+                        content: Text('Digite um e-mail válido!'),
                         duration: Duration(seconds: 3),
                       ),
                     );
                   }
                 },
-                child: Text('OK'),
+                child: Text('Gerar Senha'),
+              ),
+              SizedBox(height: 30),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue.shade100,
+                  foregroundColor: Colors.blue.shade900,
+                  minimumSize: Size(200, 50),
+                  shadowColor: Colors.red,
+                ),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('Voltar Para Login'),
               ),
             ],
           ),
@@ -88,4 +99,11 @@ bool validateEmail(String email) {
   // Regular expression pattern for a valid email address
   const pattern = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
   return RegExp(pattern).hasMatch(email);
+}
+
+String generateRandomString() {
+  final random = Random();
+  final characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#\$%^&*()_+[]{}|;:,.<>?';
+  final randomString = List.generate(6, (index) => characters[random.nextInt(characters.length)]).join();
+  return randomString;
 }
