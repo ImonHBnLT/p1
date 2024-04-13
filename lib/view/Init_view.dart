8c44ccb3c4b271116a8bc26c8d813ed5373c40ee
ImonHBnLT <angeloimon@outlook.com>
2024-04-13 16:39:06 -0300

    ok

--- lib/view/Init_view.dart
+++ lib/view/Init_view.dart
@@ -15,79 +15,87 @@ class _InitViewState extends State<InitView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Lista de Compras ${lstindex + 1}')),
+      appBar: AppBar(
+        title: Text('Minhas Listas'),
+        centerTitle: true,
+      ),
       body: ListView.builder(
         itemCount: shoppingLists.length,
         itemBuilder: (context, index) {
-          return ListTile(
-            title: GestureDetector(
-              onLongPress: () async {
-                final value = await showMenu(
-                  context: context,
-                  position: RelativeRect.fromLTRB(50.0, 50.0, 0.0, 0.0),
-                  items: <PopupMenuEntry>[
-                    PopupMenuItem(
-                      value: 'rename',
-                      child: Text('Renomear'),
-                    ),
-                    PopupMenuItem(
-                      value: 'delete',
-                      child: Text('Deletar'),
-                    ),
-                  ],
-                );
-                if (value == 'delete') {
-                  setState(() {
-                    shoppingLists.removeAt(index);
-                  });
-                } else if (value == 'rename') {
-                  final newName = await showDialog<String>(
-                    context: context,
-                    builder: (context) => Builder(
-                      builder: (innerContext) {
-                        final controller = TextEditingController();
-                        return AlertDialog(
-                          title: Text('Renomear lista'),
-                          content: TextField(
-                            controller: controller,
-                            autofocus: true,
-                            decoration: InputDecoration(
-                              labelText: 'Novo nome',
-                            ),
-                          ),
-                          actions: <Widget>[
-                            TextButton(
-                              child: Text('Cancelar'),
-                              onPressed: () {
-                                Navigator.of(innerContext).pop();
-                              },
-                            ),
-                            TextButton(
-                              child: Text('OK'),
-                              onPressed: () {
-                                Navigator.of(innerContext).pop(controller.text);
-                              },
-                            ),
-                          ],
-                        );
-                      },
-                    ),
-                  );
-                  if (newName != null) {
-                    setState(() {
-                      shoppingLists[index] = newName;
-                    });
-                  }
-                }
-              },
-              child: Text(shoppingLists[index]),
-            ),
-            onTap: () {
-              lstindex = index;
-              Navigator.pushNamed(context, 'lista',
-                  arguments: shoppingLists[index]);
-            },
-          );
+          return Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: ListTile(
+                title: GestureDetector(
+                  onLongPress: () async {
+                    final value = await showMenu(
+                      context: context,
+                      position: RelativeRect.fromLTRB(50.0, 50.0, 0.0, 0.0),
+                      items: <PopupMenuEntry>[
+                        PopupMenuItem(
+                          value: 'renomear',
+                          child: Text('Renomear'),
+                        ),
+                        PopupMenuItem(
+                          value: 'deletar',
+                          child: Text('Deletar'),
+                        ),
+                      ],
+                    );
+                    if (value == 'deletar') {
+                      setState(() {
+                        shoppingLists.removeAt(index);
+                      });
+                    } else if (value == 'renomear') {
+                      final newName = await showDialog<String>(
+                        context: context,
+                        builder: (context) => Builder(
+                          builder: (innerContext) {
+                            final controller = TextEditingController();
+                            return AlertDialog(
+                              title: Text('Renomear lista'),
+                              content: TextField(
+                                controller: controller,
+                                autofocus: true,
+                                decoration: InputDecoration(
+                                  labelText: 'Novo nome',
+                                ),
+                              ),
+                              actions: <Widget>[
+                                TextButton(
+                                  child: Text('Cancelar'),
+                                  onPressed: () {
+                                    Navigator.of(innerContext).pop();
+                                  },
+                                ),
+                                TextButton(
+                                  child: Text('OK'),
+                                  onPressed: () {
+                                    Navigator.of(innerContext)
+                                        .pop(controller.text);
+                                  },
+                                ),
+                              ],
+                            );
+                          },
+                        ),
+                      );
+                      if (newName != null) {
+                        setState(() {
+                          shoppingLists[index] = newName;
+                        });
+                      }
+                    }
+                  },
+                  child: Text(shoppingLists[index]),
+                ),
+                onTap: () {
+                  lstindex = index;
+                  Navigator.pushNamed(context, 'lista',
+                      arguments: shoppingLists[index]);
+                },
+              ));
         },
       ),
       floatingActionButton: FloatingActionButton(
--- lib/view/shoppinglist_view.dart
+++ lib/view/shoppinglist_view.dart
@@ -15,90 +15,100 @@ class _ShoppingListViewState extends State<ShoppingListView> {
   @override
   Widget build(BuildContext context) {
     final String listName =
-        ModalRoute.of(context)!.settings.arguments as String? ??
-            'Default List Name';
-
+        ModalRoute.of(context)!.settings.arguments as String? ?? 'List';
     return Scaffold(
-      appBar: AppBar(title: Text(listName)),
+      appBar: AppBar(
+        title: Text(listName),
+        centerTitle: true,
+      ),
       body: ListView.builder(
         itemCount: shoppingLists[listName]?.length ?? 0,
         itemBuilder: (context, index) {
-          return CheckboxListTile(
-            title: Text('${shoppingLists[listName]![index]['nome']}'),
-            subtitle: Text(
-                '${shoppingLists[listName]![index]['descricao']}\nQuantidade: ${shoppingLists[listName]![index]['quantidade']}'),
-            value: shoppingLists[listName]![index]['comprado'] ?? false,
-            onChanged: (bool? value) {
-              setState(() {
-                shoppingLists[listName]![index]['comprado'] = value;
-              });
-            },
-            secondary: IconButton(
-              icon: Icon(Icons.edit),
-              onPressed: () {
-                showDialog(
-                  context: context,
-                  builder: (context) {
-                    TextEditingController nomeController =
-                        TextEditingController(
-                            text: shoppingLists[listName]![index]['nome']);
-                    TextEditingController descricaoController =
-                        TextEditingController(
-                            text: shoppingLists[listName]![index]['descricao']);
-                    TextEditingController quantidadeController =
-                        TextEditingController(
-                            text: shoppingLists[listName]![index]['quantidade']
-                                .toString());
-                    return AlertDialog(
-                      title: Text('Editar Item'),
-                      content: Column(
-                        children: [
-                          TextField(
-                            controller: nomeController,
-                            decoration: InputDecoration(hintText: 'Nome'),
+          return Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: CheckboxListTile(
+                title: Text('${shoppingLists[listName]![index]['nome']}'),
+                subtitle: Text(
+                    '${shoppingLists[listName]![index]['descricao']}\nQuantidade: ${shoppingLists[listName]![index]['quantidade']}'),
+                value: shoppingLists[listName]![index]['comprado'] ?? false,
+                onChanged: (bool? value) {
+                  setState(() {
+                    shoppingLists[listName]![index]['comprado'] = value;
+                  });
+                },
+                secondary: IconButton(
+                  icon: Icon(Icons.edit),
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (context) {
+                        TextEditingController nomeController =
+                            TextEditingController(
+                                text: shoppingLists[listName]![index]['nome']);
+                        TextEditingController descricaoController =
+                            TextEditingController(
+                                text: shoppingLists[listName]![index]
+                                    ['descricao']);
+                        TextEditingController quantidadeController =
+                            TextEditingController(
+                                text: shoppingLists[listName]![index]
+                                        ['quantidade']
+                                    .toString());
+                        return AlertDialog(
+                          title: Text('Editar Item'),
+                          content: Column(
+                            children: [
+                              TextField(
+                                controller: nomeController,
+                                decoration: InputDecoration(hintText: 'Nome'),
+                              ),
+                              TextField(
+                                controller: descricaoController,
+                                decoration:
+                                    InputDecoration(hintText: 'Descrição'),
+                              ),
+                              TextField(
+                                controller: quantidadeController,
+                                decoration:
+                                    InputDecoration(hintText: 'Quantidade'),
+                                keyboardType: TextInputType.number,
+                              ),
+                            ],
                           ),
-                          TextField(
-                            controller: descricaoController,
-                            decoration: InputDecoration(hintText: 'Descrição'),
-                          ),
-                          TextField(
-                            controller: quantidadeController,
-                            decoration: InputDecoration(hintText: 'Quantidade'),
-                            keyboardType: TextInputType.number,
-                          ),
-                        ],
-                      ),
-                      actions: [
-                        TextButton(
-                          child: Text('Salvar'),
-                          onPressed: () {
-                            setState(() {
-                              shoppingLists[listName]![index]['nome'] =
-                                  nomeController.text;
-                              shoppingLists[listName]![index]['descricao'] =
-                                  descricaoController.text;
-                              shoppingLists[listName]![index]['quantidade'] =
-                                  int.parse(quantidadeController.text);
-                              Navigator.of(context).pop();
-                            });
-                          },
-                        ),
-                        TextButton(
-                          child: Text('Remover'),
-                          onPressed: () {
-                            setState(() {
-                              shoppingLists[listName]!.removeAt(index);
-                              Navigator.of(context).pop();
-                            });
-                          },
-                        ),
-                      ],
+                          actions: [
+                            TextButton(
+                              child: Text('Salvar'),
+                              onPressed: () {
+                                setState(() {
+                                  shoppingLists[listName]![index]['nome'] =
+                                      nomeController.text;
+                                  shoppingLists[listName]![index]['descricao'] =
+                                      descricaoController.text;
+                                  shoppingLists[listName]![index]
+                                          ['quantidade'] =
+                                      int.parse(quantidadeController.text);
+                                  Navigator.of(context).pop();
+                                });
+                              },
+                            ),
+                            TextButton(
+                              child: Text('Remover'),
+                              onPressed: () {
+                                setState(() {
+                                  shoppingLists[listName]!.removeAt(index);
+                                  Navigator.of(context).pop();
+                                });
+                              },
+                            ),
+                          ],
+                        );
+                      },
                     );
                   },
-                );
-              },
-            ),
-          );
+                ),
+              ));
         },
       ),
       floatingActionButton: FloatingActionButton(
@@ -142,7 +152,7 @@ class _ShoppingListViewState extends State<ShoppingListView> {
                           'nome': nomeController.text,
                           'descricao': descricaoController.text,
                           'quantidade': int.parse(quantidadeController.text),
-                          'comprado': false, // Adicione esta linha
+                          'comprado': false,
                         });
                       });
                       Navigator.pop(context);
